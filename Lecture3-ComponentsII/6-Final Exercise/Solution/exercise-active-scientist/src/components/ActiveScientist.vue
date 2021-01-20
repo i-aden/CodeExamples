781d6d4e3b3f5921252a7df6282986dda1f414ff
i-aden <i.adeniran@mmu.ac.uk>
2021-01-20 12:38:25 +0000

    A solution to the final exercise

---
+++ Lecture3-ComponentsII/6-Final Exercise/Solution/exercise-active-scientist/src/components/ActiveScientist.vue
@@ -0,0 +1,21 @@
+<template>
+  <section>
+    <h2>{{ scientistName }}</h2>
+    <h3>{{ scientistAge }} Years</h3>
+  </section>
+</template>
+
+<script>
+export default {
+  props: {
+    scientistName: {
+      type: String,
+      required: true
+    },
+    scientistAge: {
+      type: Number,
+      required: true
+    }
+  }
+};
+</script>
---
+++ Lecture3-ComponentsII/6-Final Exercise/Solution/exercise-active-scientist/src/components/ScientistData.vue
@@ -0,0 +1,26 @@
+<template>
+  <section>
+    <form @submit.prevent="submitData">
+      <input type="text" placeholder="Your name" v-model="enteredName" />
+      <input type="text" placeholder="Your age" v-model="enteredAge" />
+      <button>Set Scientist Data</button>
+    </form>
+  </section>
+</template>
+
+<script>
+import { ref } from "vue";
+export default {
+  emits: ["set-data"],
+  setup(props, context) {
+    const enteredName = ref("");
+    const enteredAge = ref("");
+
+    function submitData() {
+      context.emit("set-data", enteredName.value, enteredAge.value);
+    }
+
+    return { enteredName, enteredAge, submitData };
+  },
+};
+</script>
